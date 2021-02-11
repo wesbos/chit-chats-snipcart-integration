aff3ef99ee9c90863e545df071b5f624219de392
Wes Bos <wesbos@gmail.com>
2021-02-11 13:21:33 -0500

    okay

--- app/pages/api/shipments/index.ts
+++ app/pages/api/shipments/index.ts
@@ -6,8 +6,6 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
-    const params = new URLSearchParams(req.query);
-    console.log(req.query);
     res.status(200).json(
       Array.from({ length: Math.ceil(Math.random() * 10) }, (_, i) => ({
         id: `SHIP-${i}-BATCH-${req.query.batch_id}`,
