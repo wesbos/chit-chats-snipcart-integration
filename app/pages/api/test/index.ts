a0953343e8667a127644fa9dd8361172c8e39bf0
Wes Bos <wesbos@gmail.com>
2021-02-19 14:17:37 -0500

    hmm

---
+++ app/pages/api/test/index.ts
@@ -0,0 +1,10 @@
+import { NextApiResponse, NextApiRequest } from 'next';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  res.status(200).json({ ID: process.env.CHITCHATS_CLIENT_ID });
+}
+
+export default handler;
